2e7268391089dfbbe69644e3b22070963d485c84
Glorius King <gloriusking3@gmail.com>
2023-11-08 14:52:26 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,62 @@
 // Add your JavaScript here
+const buttons = document.querySelectorAll('.button');
+const input = document.querySelector('.display');
+
+function createCalculator(input) {
+    let currentInput = '';
+
+    function displayNumber(number) {
+        currentInput += number;
+    }
+
+    function clearInput() {
+        currentInput = '';
+    }
+
+    function deleteLastInput() {
+        currentInput = currentInput.slice(0, -1);
+    }
+
+    function equalsTo() {
+        currentInput = eval(currentInput);
+    }
+
+    function evaluatePercentage() {
+        currentInput = (parseFloat(currentInput) / 100).toString();
+    }
+
+    function showValue() {
+        input.value = currentInput;
+    }
+
+    return {
+        displayNumber,
+        clearInput,
+        deleteLastInput,
+        equalsTo,
+        evaluatePercentage,
+        showValue
+    };
+}
+
+const calculator = createCalculator(input);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const buttonText = button.innerText;
+
+        if (buttonText === 'AC') {
+            calculator.clearInput();
+        } else if (buttonText === 'DEL') {
+            calculator.deleteLastInput();
+        } else if (buttonText === '=') {
+            calculator.equalsTo();
+        } else if (buttonText === '%') {
+            calculator.evaluatePercentage();
+        } else {
+            calculator.displayNumber(buttonText);
+        }
+
+        calculator.showValue();
+    });
+});
